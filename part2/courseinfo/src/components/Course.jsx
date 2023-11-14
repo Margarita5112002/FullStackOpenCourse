5f57d106db897517e42c84dc96aab3ede55397f9
Margarita <margaritaalcarazrojas@gmail.com>
2023-11-13 18:12:03 -0700

    Exercises 2.1-2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,34 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  const total = parts.reduce((b, nextPart) => {
+    return b + nextPart.exercises;
+  }, 0);
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+      <Total sum={total} />
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+export default Course;
