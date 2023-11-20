cf0890eaaab2a4d079f14b029f3a6ab8c81ed106
Margarita <margaritaalcarazrojas@gmail.com>
2023-11-19 17:13:23 -0700

    GET Request for give information about phonebook - Exercise 3.2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,30 +2,37 @@ const express = require('express')
 const app = express()
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${persons.length} persons</p>
+    <br/>
+    <p>${date.toDateString()} ${date.toTimeString()}</p>`)
 })
 
 const PORT = 3001
