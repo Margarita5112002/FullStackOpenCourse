704f9fafc0222fea3e7f069111f9a35c9cb12cb5
Margarita <margaritaalcarazrojas@gmail.com>
2023-11-19 17:45:00 -0700

    Implement DELETE Request - Exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,8 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -44,6 +45,12 @@ app.get('/api/persons/:id', (request, response) => {
         response.statusMessage = `Can't find person with id ${id}`
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id != id)
+    response.status(204).end()
 })
 
 const PORT = 3001
