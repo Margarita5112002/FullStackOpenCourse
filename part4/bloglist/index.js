7c3752ae4399e67636d101f826548579302659b6
Margarita <margaritaalcarazrojas@gmail.com>
2023-11-30 12:56:00 -0700

    Add request to get all blogs

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -26,8 +26,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
+    Blog.find({})
         .then(blogs => {
             response.json(blogs)
         })
