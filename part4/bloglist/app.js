2145972e4aaa597eeda7eae5f343bf6bb8af477c
Margarita <margaritaalcarazrojas@gmail.com>
2023-11-30 16:39:17 -0700

    Add dummy test - Exercise 4.3

---
+++ part4/bloglist/app.js
@@ -0,0 +1,29 @@
+const {PORT, MONGODB_URI} = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+logger.info('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch(error => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
