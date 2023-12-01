36f830a3a6075ee62f2fa26deb08ba25a952dafe
Margarita <margaritaalcarazrojas@gmail.com>
2023-11-30 17:31:19 -0700

    Add favorite blog function and its tests - Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,12 +1,27 @@
 const dummy = (blogs) => 1
 
-const totalLikes = (blogs) => 
+const totalLikes = (blogs) =>
     blogs.reduce((acc, b) => {
         return acc + b.likes
     }, 0)
 
+const favoriteBlog = (blogs) => {
+    const fav = blogs.reduce((acc, b) => {
+        if (!acc || acc.likes < b.likes) return b
+        return acc
+    }, null)
+    if (fav){
+        return {
+            title: fav.title,
+            author: fav.author,
+            likes: fav.likes
+        }
+    }
+    return null
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
