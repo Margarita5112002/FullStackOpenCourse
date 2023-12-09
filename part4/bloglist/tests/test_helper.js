dc9b98e3434705f385c05f27473f7594d30ce712
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-08 17:18:20 -0700

    Test bloglist api return the correct amount of blog posts - Exercise 4.8

---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,26 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+	{
+		title: 'Type wars',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+		likes: 2
+	},
+	{
+		title: 'Canonical string reduction',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+		likes: 12
+	}
+]
+
+const blogsInDb = async () => {
+	const blogs = await Blog.find({})
+	return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = {
+	blogsInDb,
+	initialBlogs
+}
