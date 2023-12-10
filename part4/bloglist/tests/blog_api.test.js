baf8688a78879f2cae8bafd13f348004b7472d57
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-09 17:16:00 -0700

    Test add blog and refactor POST method - Exercise 4.10

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -31,6 +31,34 @@ test('blog posts have id property', async () => {
 	expect(response.body[0].id).toBeDefined()
 })
 
+test('a valid blog post can be added', async () => {
+	const newBlog = {
+		title: 'Example of blog',
+		author: 'Tom',
+		url: 'http://www.blog1.com',
+		likes: 30
+	}
+
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+	const response = await api.get('/api/blogs')
+
+	const titles = response.body.map(r => r.title)
+	const authors = response.body.map(r => r.author)
+	const urls = response.body.map(r => r.url)
+	const likes = response.body.map(r => r.likes)
+
+	expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+	expect(titles).toContain(newBlog['title'])
+	expect(authors).toContain(newBlog['author'])
+	expect(urls).toContain(newBlog['url'])
+	expect(likes).toContain(newBlog['likes'])
+})
+
 afterAll(async () => {
 	await mongoose.connection.close()
 })
