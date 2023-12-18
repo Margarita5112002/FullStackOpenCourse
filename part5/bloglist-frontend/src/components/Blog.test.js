e5ddd1fcb9038a4b9575b4ed23df116f65619dd5
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-17 17:31:26 -0700

    Test like button - Exercise 5.15

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -63,4 +63,35 @@ describe('<Blog />', () => {
 		const urlElement = screen.getByText(blog.url, { exact: false })
 		const likesElement = screen.getByText(blog.likes, { exact: false })
 	})
+	test('call likeBlog twice if user clicks like button twice', async () => {
+		const blog = {
+			title: 'Testing blog',
+			author: 'David',
+			url: 'www.abc.com',
+			likes: 40,
+			user: {
+				name: 'Magi',
+				username: 'user123',
+				id: '1234'
+			}
+		}
+
+		const mockLikeBlog = jest.fn()
+		const dummyDeleteBlog = jest.fn()
+
+		render(
+			<Blog blog={blog}
+				canDelete={false}
+				deleteBlog={dummyDeleteBlog}
+				likeBlog={mockLikeBlog} />).container
+
+		const user = userEvent.setup()
+		const viewButton = screen.getByText('View')
+		await user.click(viewButton)
+		const likeButton = screen.getByText('Like')
+		await user.click(likeButton)
+		await user.click(likeButton)
+
+		expect(mockLikeBlog.mock.calls).toHaveLength(2)
+	})
 })
