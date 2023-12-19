0156cf7b9b45ef4913c5c9385cadde837ae304a0
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-18 17:22:48 -0700

    Config cypress and test login form is shown - Exercise 5.17

---
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,13 @@
+describe('Blog app', function() {
+	beforeEach(function() {
+		cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+		cy.visit('')
+	})
+
+	it('Login form is shown', function() {
+		cy.contains('Log in to application')
+		cy.contains('Username').get('input')
+		cy.contains('Password').get('input')
+		cy.contains('Login')
+	})
+})
