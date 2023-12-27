dfb9bedd4beb847ddd767918b43cfbb3007bbb2e
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-27 15:11:31 -0700

    Implement reduxjs/toolkit and use createSlice in filter reducer - Exercise 6.10

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,14 @@
-export const changeFilter = (query) => {
-	return {
-		type: 'SET_FILTER',
-		payload: query
-	}
-}
+import { createSlice } from "@reduxjs/toolkit"
 
-const reducer = (state = '', action) => {
-	switch(action.type){
-		case 'SET_FILTER': 
+const filterSlice = createSlice({
+	name: 'filter',
+	initialState: '',
+	reducers: {
+		changeFilter(state, action){
 			return action.payload
-		default:
-			return state
+		}
 	}
-}
+})
 
-export default reducer
+export const { changeFilter } = filterSlice.actions
+export default filterSlice.reducer
