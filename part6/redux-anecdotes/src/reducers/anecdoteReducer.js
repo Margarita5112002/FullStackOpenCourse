de20b2c92e5a4491316c5e5787e3d0af3fcaa9b6
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-28 11:25:12 -0700

    Modify the creation of a new anecdote to happen using asynchronous action creators - Exercise 6.17

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -39,4 +39,11 @@ export const initializeAnecdotes = () => {
 	}
 }
 
+export const createNewAnecdote = (content) => {
+	return async dispatch => {
+		const anecdote = await anecdoteService.createNew(content)
+		dispatch(newAnecdote(anecdote))
+	}
+}
+
 export default anecdoteSlice.reducer
