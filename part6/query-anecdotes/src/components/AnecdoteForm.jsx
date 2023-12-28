652f9d184e6a7e927f2357a3d44f9aa6e11630ad
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-28 13:40:57 -0700

    Implement adding new anecdotes to the server using React Query - Exercise 6.21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,34 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import anecdoteService from "../services/anecdotes"
+
 const AnecdoteForm = () => {
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+	const queryClient = useQueryClient()
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
+	const newAnecdoteMutation = useMutation({
+		mutationFn: anecdoteService.createNew,
+		onSuccess: (newAnecdote) => {
+			const anecdotes = queryClient.getQueryData(['anecdotes'])
+			queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+		}
+	})
+
+	const onCreate = (event) => {
+		event.preventDefault()
+		const content = event.target.anecdote.value
+		event.target.anecdote.value = ''
+		newAnecdoteMutation.mutate(content)
+	}
+
+	return (
+		<div>
+			<h3>create new</h3>
+			<form onSubmit={onCreate}>
+				<input name='anecdote' />
+				<button type="submit">create</button>
+			</form>
+		</div>
+	)
 }
 
 export default AnecdoteForm
