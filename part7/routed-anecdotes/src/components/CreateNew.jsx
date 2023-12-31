03c4dda70595df31e567eee9cee9f1a7adf8d4fa
Margarita <margaritaalcarazrojas@gmail.com>
2023-12-30 18:49:54 -0700

    Implement useField hook in create new form - Exercise 7.4

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from "../hooks"
 
 const CreateNew = (props) => {
-	const [content, setContent] = useState('')
-	const [author, setAuthor] = useState('')
-	const [info, setInfo] = useState('')
+	const content = useField('text')
+	const author = useField('text')
+	const info = useField('text')
 	const navigate = useNavigate()
 
 	const showNotification = (content, displayInSeconds) =>{ 
@@ -17,12 +18,12 @@ const CreateNew = (props) => {
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		props.addNew({
-			content,
-			author,
-			info,
+			content: content.value,
+			author: author.value,
+			info: info.value,
 			votes: 0
 		})
-		showNotification(content, 5)
+		showNotification(content.value, 5)
 		navigate('/')
 	}
 
@@ -32,15 +33,15 @@ const CreateNew = (props) => {
 			<form onSubmit={handleSubmit}>
 				<div>
 					content
-					<input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+					<input {... content}/>
 				</div>
 				<div>
 					author
-					<input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+					<input {... author}/>
 				</div>
 				<div>
 					url for more info
-					<input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+					<input {... info}/>
 				</div>
 				<button>create</button>
 			</form>
