8b4ca66042f73d1beeb1d9b090fe4d15dce942f0
Margarita <margaritaalcarazrojas@gmail.com>
2024-01-16 22:22:07 -0700

    implement /api/patients get endpoint - Exercise 9.11

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors';
 import diagnosisRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
 
 const PORT = 3001;
 
@@ -13,6 +14,7 @@ app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
 app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -4,4 +4,15 @@ export interface Diagnosis {
     latin?: string
 }
 
+export interface Patient {
+    id: string,
+    name: string,
+    dateOfBirth: string,
+    ssn: string,
+    gender: string,
+    occupation: string
+}
+
+export type NonSensitivePatient = Omit<Patient, 'ssn'>;
+
 export default 'default';
