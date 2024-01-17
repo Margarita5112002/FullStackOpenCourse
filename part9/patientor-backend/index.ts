f48b3c2bda147eb6c341a5b8280246b6ab581690
Margarita <margaritaalcarazrojas@gmail.com>
2024-01-16 21:15:01 -0700

    frontend can connect with backend - Exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from 'cors';
 
 const PORT = 3001;
 
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
     res.send('pong');
