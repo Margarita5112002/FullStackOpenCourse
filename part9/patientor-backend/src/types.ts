1efb65f2380c8a11ec2a7cef61a1224447e4b6a2
Margarita <margaritaalcarazrojas@gmail.com>
2024-01-19 21:44:53 -0700

    Define the types OccupationalHealthcareEntry and HospitalEntry - Exercise 9.22

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -10,20 +10,62 @@ export enum Gender {
     Other = 'other'
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+    description: string;
+    discharge: {
+        date: string;
+        criteria: string;
+    }
+    type: 'Hospital';
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: {
+        startDate: string,
+        endDate: string,
+    }
+}
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
 
 export interface Patient {
-  id: string;
-  name: string;
-  ssn: string;
-  occupation: string;
-  gender: Gender;
-  dateOfBirth: string;
-
-  entries: Entry[]
+    id: string;
+    name: string;
+    ssn: string;
+    occupation: string;
+    gender: Gender;
+    dateOfBirth: string;
+    entries: Entry[]
 }
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
 
